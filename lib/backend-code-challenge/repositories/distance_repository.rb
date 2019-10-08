a8eb1e98e5e1daf569d670e0692f8692191bb720
Wellison Souza <wellison28@gmail.com>
2019-10-08 07:47:10 -0300

    feat: create methods to search items

--- lib/backend-code-challenge/repositories/distance_repository.rb
+++ lib/backend-code-challenge/repositories/distance_repository.rb
@@ -1,2 +1,16 @@
 class DistanceRepository < Hanami::Repository
+  def origin(origin)
+    distances.where(origin: origin).to_a
+  end
+
+  def destination(destination)
+    distances.where(destination: destination).to_a
+  end
+
+  def origin_destination(origin, destination)
+    [
+      distances.where(origin: origin, destination: destination).to_a,
+      distances.where(origin: destination, destination: origin).to_a
+    ].flatten
+  end
 end
