1ab2c4bffb25bad3fc236b4d69edce7599936b21
Wellison Souza <wellison28@gmail.com>
2019-10-09 18:09:52 -0300

    refact: change project to sinatra

--- lib/backend-code-challenge/entities/distance.rb
+++
@@ -1,8 +0,0 @@
-class Distance < Hanami::Entity
-  attributes do
-    attribute :id,           Types::Int
-    attribute :origin,       Types::String
-    attribute :destination,  Types::String
-    attribute :distance,     Types::Int.constrained(gt: 0, lt: 100001)
-  end
-end
