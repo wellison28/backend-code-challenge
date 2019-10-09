4501b5c4dc777f711b338a5f217b47874378c79c
Wellison Souza <wellison28@gmail.com>
2019-10-09 07:59:58 -0300

    feat: add tests

--- spec/backend-code-challenge/entities/distance_spec.rb
+++ spec/backend-code-challenge/entities/distance_spec.rb
@@ -1,3 +1,8 @@
 RSpec.describe Distance, type: :entity do
-  # place your tests here
+  it 'can be initialized with attributes' do
+    book = Distance.new(origin: 'A', distance: 10, destination: 'B')
+    expect(book.origin).to eq('A')
+    expect(book.destination).to eq('B')
+    expect(book.distance).to eq(10)
+  end
 end
