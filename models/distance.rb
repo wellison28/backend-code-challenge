3a1097125892deea37a825e252f15e4c3608ef94
Wellison Souza <wellison28@gmail.com>
2019-10-09 22:42:00 -0300

    feat: add constrains to model distance

--- models/distance.rb
+++ models/distance.rb
@@ -1,2 +1,6 @@
 class Distance < ActiveRecord::Base
+  validates :origin, length: { minimum: 1 }
+  validates :destination, length: { minimum: 1 }
+  validates :kilometers, numericality: { greater_than: 0 }
+  validates :kilometers, numericality: { less_than_or_equal_to: 100000 }
 end
