14f1dda8a8d3e7f8e65cc96fdd2ae3b82f12a1f5
Wellison Souza <wellison28@gmail.com>
2019-10-09 23:42:05 -0300

    feat: change to invironment variables

--- config/puma.rb
+++ config/puma.rb
@@ -1,9 +1,9 @@
-# workers Integer(ENV['CONCURRENCY'] || 1)
-# threads_count = Integer(ENV['MAX_THREADS'] || 5)
-# threads threads_count, threads_count
+workers Integer(ENV['CONCURRENCY'] || 1)
+threads_count = Integer(ENV['MAX_THREADS'] || 5)
+threads threads_count, threads_count
 
-# preload_app!
+preload_app!
 
-# rackup      DefaultRackup
-# port        ENV['PORT']     || 4567
-# environment ENV['RACK_ENV'] || 'production'
+rackup      DefaultRackup
+port        ENV['PORT']     || 4567
+environment ENV['RACK_ENV'] || 'production'
