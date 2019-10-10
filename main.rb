2ed05ce5344e2c9dfc4fb7c2d93de6494ff306b2
Wellison Souza <wellison28@gmail.com>
2019-10-09 22:44:02 -0300

    feat: implement crud

--- main.rb
+++ main.rb
@@ -3,14 +3,42 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'sinatra/activerecord'
 require_relative 'models/distance'
+require_relative 'lib/diikstra/edge'
+require_relative 'lib/diikstra/graph'
+require_relative 'lib/services/utils'
 
 class App < Sinatra::Base
-  
+  before do
+    content_type 'application/json'
+  end
+
   get "/cost" do
-    "Hello world! #{params}"
+    distance = Service::get_lowest_path(params)
+    if Service::valid_params?(params)
+      if distance.nil?
+        status 404
+        {messsage: "Path not exists!"}
+      else
+        status 201
+        Service::calculate_cost(distance, params) 
+      end
+    else
+      status 400
+      {message: "Weight value invalid!"}
+    end    
   end
 
   post "/distance" do
-    "Hello world!"
+    params = request.body.read
+    origin, destination, distance = Service::parse_values(params)
+    saved_result = Service::save_values(origin, destination, distance)
+
+    if saved_result
+      status 200
+      { message: 'Distance created with success!' }.to_json
+    else
+      status 400
+      { message: 'Error on create or update distance!' }.to_json
+    end
   end
 end
