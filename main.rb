a44372d181e36e4eae24e954cdf9bad8d84b05f6
Wellison Souza <wellison28@gmail.com>
2019-10-09 21:48:37 -0300

    feat: create routes

--- main.rb
+++ main.rb
@@ -1,9 +1,16 @@
+require 'pry'
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'sinatra/activerecord'
+require_relative 'models/distance'
 
 class App < Sinatra::Base
-  get "/" do
+  
+  get "/cost" do
+    "Hello world! #{params}"
+  end
+
+  post "/distance" do
     "Hello world!"
   end
 end
