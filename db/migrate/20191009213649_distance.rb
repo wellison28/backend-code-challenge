467e50e76402df656d2753d01612567a011c24e1
Wellison Souza <wellison28@gmail.com>
2019-10-09 21:47:12 -0300

    feat: add migrate to create distance table

--- db/migrate/20191009213649_distance.rb
+++ db/migrate/20191009213649_distance.rb
@@ -1,4 +1,9 @@
 class Distance < ActiveRecord::Migration[5.2]
   def change
+    create_table :distances do |t|
+      t.string :origin, null: false
+      t.string :destination, null: false
+      t.integer :kilometers, null: false
+    end
   end
 end
